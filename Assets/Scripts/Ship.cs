e4045236a67ef75a81b2a38765f576e0f8e9ab57
oalberto96 <oalberto96c@gmail.com>
2018-02-10 11:36:10 -0800

    Limit move x distance player

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class Ship : MonoBehaviour {
 
 	public float speed = 5.0f;
+	public float padding = 1;
+	float xmin;
+	float xmax;
 
 	// Use this for initialization
 	void Start () {
-		
+		float distance = transform.position.z - Camera.main.transform.position.z;	
+		//Left bottom is (0,0), Right top us (1,1)
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if(Input.GetKey(KeyCode.RightArrow)){
-			transform.position += new Vector3(+speed * Time.deltaTime, 0, 0);		}
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		// Restrict the player to the gamespace
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
