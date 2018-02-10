537214e12828c20edf73560cefd97436f14d82a5
oalberto96 <oalberto96c@gmail.com>
2018-02-10 10:38:23 -0800

    Move player

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		} else if(Input.GetKey(KeyCode.RightArrow)){
+			transform.position += new Vector3(+speed * Time.deltaTime, 0, 0);		}
+	}
+}
