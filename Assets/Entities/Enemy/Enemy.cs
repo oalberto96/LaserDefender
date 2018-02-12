c1a0ef6c2f99c1b0596eadad1b2c8e69f3d3d88d
oalberto96 <oalberto96c@gmail.com>
2018-02-11 20:08:27 -0800

    Shooting enemies

---
+++ Assets/Entities/Enemy/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float life = 10f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Projectile missile = other.gameObject.GetComponent<Projectile>();
+        if (missile)
+        {
+            missile.Hit();
+            life -= missile.damage;
+            if (life < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
